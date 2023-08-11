0f9cba8d30122ce3d3477609c5789c67779618c9
Daedalus <manny.is.daedalus@pm.me>
2023-08-11 09:23:38 +0000

    reorganised some files and added new keymaps

--- lua/daedalus/set.lua
+++ lua/daedalus/set.lua
@@ -1,23 +1,3 @@
-vim.cmd('colorscheme deus')
-vim.opt.guicursor = ""
--- leader key for vim
-vim.g.mapleader = ' '
-
--- enter netrw
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
--- imported from theprimeagen
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
--- neat keymaps again stolen from primeagen
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzz")
-vim.keymap.set("n", "N", "Nzz")
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
 -- editor options
 vim.cmd('set cursorline')
 vim.opt.nu = true
